bcf1c2d3ba736a08a20f61f848c4754a02d7de60
Vladyslav Shuliar <vbshuliar@gmail.com>
2025-12-06 22:27:14 +0000

    finish main menu with options and tutorial

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -1,57 +1,107 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
+// handles tutorial screen buttons for starting levels or returning to main menu
 public class TutorialController : MonoBehaviour
 {
-    [Header("Tutorial Screen")]
-    [SerializeField] private GameObject tutorialScreen;
+    [Header("UI Buttons")]
+    [SerializeField] private Button startLevel1Button;
+    [SerializeField] private Button startLevel2Button;
+    [SerializeField] private Button mainMenuButton;
 
-    [Header("Settings")]
-    [SerializeField] private Button skipButton;
-
-    private bool tutorialCompleted = false;
+    [Header("Scene Names")]
+    [SerializeField] private string level1SceneName = "Level1";
+    [SerializeField] private string level2SceneName = "Level2";
+    [SerializeField] private string mainMenuSceneName = "MainMenu";
 
     void Start()
     {
-        // Show tutorial screen initially
-        if (tutorialScreen != null)
+        // setup button listeners
+        SetupButton(startLevel1Button, OnStartLevel1Clicked, "Start Level 1");
+        SetupButton(startLevel2Button, OnStartLevel2Clicked, "Start Level 2");
+        SetupButton(mainMenuButton, OnMainMenuClicked, "Main Menu");
+
+        Time.timeScale = 1f;
+    }
+
+    void SetupButton(Button button, UnityEngine.Events.UnityAction action, string buttonName)
+    {
+        if (button == null)
         {
-            tutorialScreen.SetActive(true);
+            Debug.LogWarning($"TutorialController: {buttonName} button is not assigned!");
+            return;
         }
 
-        if (skipButton != null)
+        // ensure button is interactable
+        if (!button.interactable)
         {
-            skipButton.onClick.AddListener(SkipTutorial);
+            Debug.LogWarning($"TutorialController: {buttonName} button is not interactable! Enabling it...");
+            button.interactable = true;
+        }
+
+        // remove existing listeners to avoid duplicates
+        button.onClick.RemoveAllListeners();
+        
+        // add listener
+        button.onClick.AddListener(action);
+    }
+
+    public void OnStartLevel1Clicked()
+    {
+        Debug.Log("TutorialController: OnStartLevel1Clicked called!");
+        if (SceneTransitionManager.Instance != null)
+        {
+            SceneTransitionManager.Instance.LoadScene(level1SceneName);
+        }
+        else
+        {
+            SceneManager.LoadScene(level1SceneName);
         }
     }
 
-    public void SkipTutorial()
+    public void OnStartLevel2Clicked()
     {
-        // Hide tutorial screen
-        if (tutorialScreen != null)
+        Debug.Log("TutorialController: OnStartLevel2Clicked called!");
+        if (SceneTransitionManager.Instance != null)
         {
-            tutorialScreen.SetActive(false);
+            SceneTransitionManager.Instance.LoadScene(level2SceneName);
         }
-
-        tutorialCompleted = true;
-
-        // Start the game
-        if (GameManager.Instance != null)
+        else
         {
-            GameManager.Instance.StartGame();
+            SceneManager.LoadScene(level2SceneName);
         }
     }
 
-    public bool IsTutorialCompleted()
+    public void OnMainMenuClicked()
     {
-        return tutorialCompleted;
+        Debug.Log("TutorialController: OnMainMenuClicked called!");
+        if (SceneTransitionManager.Instance != null)
+        {
+            SceneTransitionManager.Instance.LoadScene(mainMenuSceneName);
+        }
+        else
+        {
+            SceneManager.LoadScene(mainMenuSceneName);
+        }
     }
 
     void OnDestroy()
     {
-        if (skipButton != null)
+        // clean up button listeners
+        if (startLevel1Button != null)
         {
-            skipButton.onClick.RemoveAllListeners();
+            startLevel1Button.onClick.RemoveAllListeners();
+        }
+
+        if (startLevel2Button != null)
+        {
+            startLevel2Button.onClick.RemoveAllListeners();
+        }
+
+        if (mainMenuButton != null)
+        {
+            mainMenuButton.onClick.RemoveAllListeners();
         }
     }
 }
